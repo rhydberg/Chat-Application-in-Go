fc30d7466b109f1c9bc14bf74a07faaac8e9cdbf
Ridin Datta <ridindatta12@gmail.com>
2024-05-07 10:16:57 +0530

    added prod dsn

--- db/db.go
+++ db/db.go
@@ -4,9 +4,9 @@ import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
-
-
-const dsn = "postgres://rhydberg:rhydbpass@localhost:5432/chat_app"
+// const dsn = "postgres://rhydberg:fDsR9Kq0GZLXM2Sc37nqFriqBTFCvMuU@dpg-conephq1hbls73fhl92g-a.oregon-postgres.render.com/rhydberg"
+const dsn = "postgres://rhydberg:fDsR9Kq0GZLXM2Sc37nqFriqBTFCvMuU@dpg-conephq1hbls73fhl92g-a.oregon-postgres.render.com/rhydberg"
+// const devdsn = "postgres://rhydberg:rhydbpass@localhost:5432/chat_app"
 
 func Init() *gorm.DB {
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
@@ -16,4 +16,4 @@ func Init() *gorm.DB {
 
 	return db
 }
-	
+	// 
